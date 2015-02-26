078764191e429be57add62a9c83a46cc3a4c0aa9
Conmy <d.conmy1@gmail.com>
2015-02-26 15:38:48 +0000

    Create ArrayFunctions.js

---
+++ ArrayFunctions.js
@@ -0,0 +1,69 @@
+// ================================================================================================
+// Name		:	Array Functions
+// Author	:	David Conmy
+// ================================================================================================
+function getMMArrayAsTableHTML(array, M){
+	var text = "<table>";
+	for(var i=0;i<M;i++){
+		text += "<tr>";
+		for(var j=0;j<M;j++){
+			text += "<td>";
+			text += array[i][j].toString();
+			text += "</td>";
+		}
+		text += "</tr>";
+	}
+	text += "</table>";
+	
+	return text;
+}
+
+function getMMArrayAsColorBlock(array, M){
+	var text = "<table>";
+	for(var i=0;i<M;i++){
+		text += "<tr>";
+		for(var j=0;j<M;j++){
+			text += "<td>";
+			text += "<div class='colorBlock' style='background-color:" + array[i][j].toString() +"'></div>";
+			text += "</td>";
+		}
+		text += "</tr>";
+	}
+	text += "</table>";
+	
+	return text;
+}
+
+function rotateMMArray(array, M){
+	
+	var newArray = new Array(M);
+	for(var i=0;i<M;i++){
+		newArray[i] = new Array(M);
+	}
+	
+	for(var a=0;a<M;a++){
+		for(var b=M-1;b>=0;b--){
+			var q = M - a - 1;
+			var p = b;
+			
+			newArray[p][q] = array[a][b];
+		}
+	}
+	
+	return newArray;
+}
+
+function copyMMArray(array, M){
+	
+	var newArray = new Array(M);
+	for(var i=0;i<M;i++){
+		newArray[i] = new Array(M);
+	}
+	
+	for(var a=0;a<M;a++){
+		for(var b=0; b<M; b++){
+			newArray[a][b] = array[a][b];
+		}
+	}
+	return newArray;
+}
